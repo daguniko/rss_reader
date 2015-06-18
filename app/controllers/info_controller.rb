0fba5bb0efdf4617e054692599dcea6985ff1ecf
Sudo <key_mtrgtring@yahoo.co.jp>
2015-06-19 05:00:10 +0900

    ビューの設定,ルーティングの設定など

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,5 +1,14 @@
 class InfoController < ApplicationController
+  before_action :set_item, only: [:detail]
   def index
     @items = Item.all
   end
+  def detail
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'info/index'
+  get '/detail/:id' => "info#detail", :as => :detail
+
+  #get 'info/detail/:id'
+  #get 'products/:id' => 'catalog#view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,7 +12,7 @@ Rails.application.routes.draw do
   root 'info#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
