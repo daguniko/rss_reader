c9c584fe00549a3deb06c4928277a72006610994
Sudo <key_mtrgtring@yahoo.co.jp>
2015-06-21 20:24:42 +0900

    procfileの修正,environmentの追加

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+ENV['LD_LIBRARY_PATH'] ||="/usr/lib"
+ENV['LD_LIBRARY_PATH'] +=":/app/lib/native"
+ENV['MECAB_PATH']='/usr/local/lib/libmecab.so'
 
 # Initialize the Rails application.
 Rails.application.initialize!
